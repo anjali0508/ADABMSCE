0dcc13c4b987aabc77f18b535fc3a7eed86b1fea
A <46600043+anjali0508@users.noreply.github.com>
2019-10-13 21:42:35 +0530

    Update DFSConnectedGraph

--- DFSstack.cpp
+++ DFSstack.cpp
@@ -1,51 +1,38 @@
 #include<iostream>
-#include<vector>
-#include<stack>
 using namespace std;
-int main(){
-	int edges;
-	int graph[5][5];
-	for(int i=0;i<5;i++){
-		for(int j=0;j<5;j++){
-			graph[i][j] = 0;
-		}
-	}
-	cout<<"Enter the number of edges";
-	cin>>edges;
-	int u, v;
-	for(int i=0;i<edges;i++){
-		cout<<"Enter the two vertices";
-		cin>>u>>v;
-		graph[u][v] = 1;
-		graph[v][u] = 1;
-	}
-	for(int i=0;i<5;i++){
-		for(int j=0;j<5;j++){
-			cout<<graph[i][j]<<" ";
-		}
-		cout<<endl;
-	}
-	stack<int> s;
-	vector<bool> visited(5, false);
-	int x;
-	for(int k=0;k<5;k++){
-		if(!visited[k]){
-			//cout<<"k:"<<k<<endl;
-			s.push(k);
-			while(!s.empty()){
-				x = s.top();
-				s.pop();
-				for(int i=0;i<5;i++){
-					if(graph[x][i]==1 && visited[i]==false){
-						cout<<i<<" ";
-						visited[i] = true;
-						s.push(i);
-					}
-				}
-			}
-			cout<<endl;
-		}
-	}
-	
-}
+int a[20][20],visited[20],n;
 
+void DFS(int v)
+{
+	cout<<v<<" ";
+	visited[v]=1;
+	for(int i=1;i<n;i++)
+	{
+		if(a[v][i]==1 && visited[i]==0)
+			DFS(i);
+	}
+}
+int main()
+{
+	
+	cout<<"\nEnter the no of vertices:";
+	cin>>n;
+	cout<<"\nEnter the adjacency matrix:";
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<n;j++)
+			cin>>a[i][j];
+		visited[i]=0;
+	}
+	cout<<"\nConnected components:\n";
+	DFS(0);
+	for(int i=0;i<n;i++)
+	{
+		if(visited[i]!=1)
+			{
+				cout<<"\n";
+				DFS(i);
+			}
+	}
+	return 0;
+}
